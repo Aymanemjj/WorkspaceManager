5f705ffca098326d285b1721fa2eef21416d01df
Aymanemjj <aymanemj588@gmail.com>
2025-11-19 15:44:17 +0100

    Add:     -Function to add workers     -completed the addtolocalstorage Function     -created loadLocalStorage Function     -update the workers sidebar function

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
-document.getElementById("addWorkerBtn").addEventListener("click", openModal1);
-document
-    .querySelectorAll(".workerCard")
-    .forEach((element) => element.addEventListener("click", openModal2));
+
+/* Linking Objects */
     
-document
-    .querySelectorAll(".closeModalBtn")
-    .forEach((element) => element.addEventListener("click", closeModal));
 
 const addModal = document.getElementById("addModal");
-
 const closeModalBtn = document.getElementById("closeModalBtn1");
-
 const profileModal = document.getElementById("profileModal");
 
-document.getElementById("addWorkerBtn");
 
 
 
-
+/* Modal controlls */
+document.getElementById("addWorkerBtn").addEventListener("click", openModal1);
 
 function openModal1() {
     addModal.classList.replace("hidden", "block");
 }
 
-function openModal2() {
+document
+    .querySelectorAll(".workerCard")
+    .forEach((element) => element.addEventListener("click", openModal2));
+
+function openModal2(element) {
     console.log("enter");
+    let filter = element.querySelector("small").innerHTML
 
     profileModal.classList.replace("hidden", "block");
 }
+
+document
+    .querySelectorAll(".closeModalBtn")
+    .forEach((element) => element.addEventListener("click", closeModal));
+
 
 function closeModal(event) {
     let temp =
@@ -35,32 +38,63 @@ function closeModal(event) {
     temp.classList.replace("block", "hidden");
 }
 
-function addToLocalStorage() {
-    workersList = [];
-    localStorage.setItem("workersList",JSON.stringify(workersList));
-    updateWorkersSection();
+/* LocalStorage */
+function addToLocalStorage(list) {
+    localStorage.setItem("workersList",JSON.stringify(list));
+    laodLocalStorage()
+}
+const workersListSection= document.getElementById("workersListSection") 
+
+function laodLocalStorage(){
+    let workersList =JSON.parse (localStorage.getItem("workersList"))
+    return workersList; 
 }
 
 function updateWorkersSection() { 
-
-}
-
-function addNewWorker(){
-    let worker = new Object()
-
-    addModal.querySelectorAll(".forumInput").forEach(element => {
-        let person ={
-            pfp : `${element.pfp}`,
-            name : `${element.fullname}`,
-            role : `${element.role}`,
-            number : `${element.number}`,
-            email : `${element.email}`,
-            experience : `${element.experience}`
-            
-        }
-        return(person)
-    })
-    worker.person;
-    console.log(worker);
+    let workersList = laodLocalStorage()
+    workersList.forEach(worker =>
+        workersListSection.innerHTML+=
+            `<div class="workerCard bg-[#A4BCC6] border-2 border-[#1E1E1E] rounded-md flex justify-between gap-2 p-1 cursor-pointer">
+                    <div class="flex gap-2">
+                        <div class="bg-[#FAFCEE] border-2 border-[#1E1E1E] rounded-sm size-12"></div>
+                        <div class="flex flex-col">
+                            <small>${worker.fullname}</small>
+                            <small>${worker.role}</small>
+                        </div>
+                    </div>
+                    <button  class="addToBoardBtn hover:bg-[#8EA1B8] cursor-pointer rounded-sm"><i class="fa-regular fa-square-plus" style="color: #1e1e1e;"></i></button>
+                </div>`
+        
+    )
     
 }
+
+/* AddWorker */
+document.getElementById("addBtn").addEventListener("click",addNewWorker);
+
+function addNewWorker(){    
+    let workersList = laodLocalStorage()
+
+    let person={
+        id: `${document.getElementById("number").value}`*2,
+        fullname: `${document.getElementById("fullname").value}`,
+        role: `${document.getElementById("role").value}`,
+        number: `${document.getElementById("number").value}`,
+        email: `${document.getElementById("email").value}`,
+        experience: `${document.getElementById("experience").value}`,
+    }
+    document.querySelectorAll("input").forEach(input=> input.value="")
+
+    workersList.push(person);
+    addToLocalStorage(workersList)
+    
+}
+document.getElementById("addExperienceBtn").addEventListener("click",addExp)
+function addExp(){
+    console.log("enter");
+    
+    document.getElementById("expDiv").innerHTML+=
+        '<input id="experience" type="text" placeholder="Experience" class="forumInput bg-[#8EA1B8] p-2 border-[#1e1e1e] border-2 rounded-sm">'
+
+}
+    updateWorkersSection();
